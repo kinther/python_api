359a2583439e9d6f758038ae79c6dd30697b6f01
kinther <xdelaranx@gmail.com>
2022-01-16 10:10:14 -0800

    Update test.py

--- test.py
+++ test.py
@@ -7,5 +7,5 @@ import json
 r = requests.get("https://covid19-api.com/country?name=USA&format=json")
 
 # present data in JSON format
-data = json.loads(r.json())
+data = json.loads(r.content)
 print(json.dumps(data, indented=4, sort_keys=True))
