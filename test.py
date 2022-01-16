5ecb00af452082ca153d2622e35ec6091aed1181
kinther <xdelaranx@gmail.com>
2022-01-16 10:09:27 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 # Testing random internet REST API's
 
 import requests
+import json
 
+# actually pull the data
 r = requests.get("https://covid19-api.com/country?name=USA&format=json")
-print(r.content)
-print(r.headers)
-print(r.json())
-print(r.request)
+
+# present data in JSON format
+data = json.loads(r.json())
+print(json.dumps(data, indented=4, sort_keys=True))
